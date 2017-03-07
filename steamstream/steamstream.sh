75eb3c0f619dfd79718269e8b937e038d65597c8
tokko <andreg@kth.se>
2017-03-07 20:04:03 +0000

    stop/start docker containers when streaming

--- steamstream/steamstream.sh
+++ steamstream/steamstream.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+docker stop $(docker ps -aq)
 sudo service kodi stop
 echo "on 0" | cec-client -s
 moonlight stream -app Steam -1080 -60fps -localaudio
 sudo service kodi start
+docker start $(docker ps -aq)
 echo "on 0" | cec-client -s
